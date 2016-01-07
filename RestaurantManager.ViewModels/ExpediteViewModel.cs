12b84850647d2c4a63044b16e9deda515500c1db
David Hughes <hughesd@paslc.org>
2016-01-06 21:09:25 -0500

    Implemented a clear orders command

--- RestaurantManager.ViewModels/ExpediteViewModel.cs
+++ RestaurantManager.ViewModels/ExpediteViewModel.cs
@@ -1,11 +1,25 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
 using RestaurantManager.Models;
+using System.Windows.Input;
+using System;
+using System.Collections.ObjectModel;
 
 namespace RestaurantManager.ViewModels
 {
     public class ExpediteViewModel : ViewModel
     {
+        public ExpediteViewModel()
+        {
+            ClearOrdersCommand = new DelegateCommand<object>(ClearOrders);
+
+        }
+
+        public void ClearOrders(object obj)
+        {
+            base.Repository?.Orders.Clear();
+            OnPropertyChanged("OrderItems");
+        }
 
         protected override void OnDataLoaded()
         {
@@ -13,7 +27,14 @@ namespace RestaurantManager.ViewModels
 
         }
 
-        public List<Order> OrderItems
+        public ICommand ClearOrdersCommand { get; private set; }
+
+        //public List<Order> OrderItems
+        //{
+        //    get { return base.Repository?.Orders; }
+        //}
+
+        public ObservableCollection<Order> OrderItems
         {
             get { return base.Repository?.Orders; }
         }
